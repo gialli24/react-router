a39503a87caf59aed4c5cd1c41e507dd68cc56f6
gialli24 <a.gialli2409@gmail.com>
2026-03-20 14:58:26 +0100

    add contacts section to Chi Siamo page and update styles

--- src/pages/ChiSiamo.jsx
+++ src/pages/ChiSiamo.jsx
@@ -1,7 +1,25 @@
 export default function ChiSiamo() {
     return (
         <main>
-            <h1>Chi Siamo</h1>
+            <section id="contacts">
+                <div className="app-container">
+                    <h2 className="text-center">Chi Siamo</h2>
+
+                    <div className="row row-cols-1 row-cols-md-2">
+                        <div className="col p-4">
+                            <h3>La nostra Storia</h3>
+
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt adipisci illo eius maxime nisi! Corporis quisquam, ullam eligendi, vel sequi esse quidem tempora nisi, sed facilis eveniet assumenda quibusdam ducimus.</p>
+                        </div>
+
+                        <div className="col p-4">
+                            <h3>Contattaci</h3>
+
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt adipisci illo eius maxime nisi! Corporis quisquam, ullam eligendi, vel sequi esse quidem tempora nisi, sed facilis eveniet assumenda quibusdam ducimus.</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </main>
     );
 }
