95b05c35d885d816d9311cc01d9d7ca49d968b20
gialli24 <a.gialli2409@gmail.com>
2026-03-20 13:02:17 +0100

    add appheader styling

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,14 +1,23 @@
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 export default function AppHeader() {
     return (
-        <header>
-            <h1>React Router Store</h1>
+        <header id="app-header">
+            <h1>Shop</h1>
 
-            <nav>
+            <nav id="pagination-nav">
                 <NavLink to="/" >Home</NavLink>
                 <NavLink to="/about-us" >Chi Siamo</NavLink>
                 <NavLink to="/products" >Prodotti</NavLink>
+            </nav>
+
+            <nav id="op-nav">
+                <Link to="/cart">
+                    <i className="bi bi-cart2"></i>
+                </Link>
+                <Link to="/profile">
+                    <i className="bi bi-person-circle"></i>
+                </Link>
             </nav>
         </header>
     );
