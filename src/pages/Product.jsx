1d846438a9773352213854670684a61a403aeaf7
gialli24 <a.gialli2409@gmail.com>
2026-03-23 15:04:36 +0100

    add porduct not found redircet to products page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 export default function Product() {
 
     const { id } = useParams();
+
+    const navigate = useNavigate();
 
     const [productData, setProductData] = useState([]);
 
@@ -13,6 +15,7 @@ export default function Product() {
         fetch(endpoint)
             .then(res => res.json())
             .then(data => setProductData(data))
+            .catch(err => navigate("/products"))
     }
 
 
@@ -20,24 +23,26 @@ export default function Product() {
         fetchProduct(productEndpoint);
     }, []);
 
-
-    console.log(productData)
-
     return (
         <main>
             <div className="app-container">
                 <div className="row row-cols-1 row-cols-md-2">
+
                     <div className="col d-flex justify-content-center">
                         <img src={productData.image} alt="" />
                     </div>
-                    <div className="col">
+
+                    <div className="col product-details">
                         <span>{productData.category}</span>
 
-                        <h2>{productData.title}</h2>
+                        <h2 className="mt-4">{productData.title}</h2>
 
                         <p>{productData.description}</p>
 
-                        <span>{productData.price}</span>
+                        <div>{productData.price}</div>
+
+                        <button className="app-btn primary" >Add to Cart</button>
+
                     </div>
                 </div>
             </div>
